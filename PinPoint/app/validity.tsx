934cc7f5a09ed0cf8c39fa837a3e3a29bcfcaec5
Moon2777 <140862329+Moon2777@users.noreply.github.com>
2025-05-12 22:28:01 -0700

    Add files via upload

--- PinPoint/app/validity.tsx
+++ PinPoint/app/validity.tsx
@@ -1,3 +1,31 @@
+//import express, { Router } from 'express';
+//import { pool } from './test.js'
+//const HOST = ""
+
+const addEntryValidity = (user: string) =>{
+  try{
+    fetch(HOST + "/tasks/addUser", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: user,
+      })
+    }).then((response) =>{
+      response.json().then((data)=>{
+        if (response.status == 201){
+          console.log("Success")
+        }else{
+          console.log("Error")
+        }
+      })
+    })
+  }catch(e){
+    console.log(e)
+  }
+}
+
 const endorsePin = (user: string, pid: string) =>{
   try{
     fetch(HOST + "/tasks/add", {
@@ -24,8 +52,10 @@ const endorsePin = (user: string, pid: string) =>{
   }
 }
 
-const unendorsePin = (user: number, pid: number) =>{
+const unendorsePin = (user: string, pid: string) =>{
+
   try{
+
     fetch(HOST + "/tasks/delete", {
       method: "POST",
       headers: {
@@ -35,7 +65,9 @@ const unendorsePin = (user: number, pid: number) =>{
         user: user,
         pin:  pid,
       })
+
     }).then((response) => {
+
       response.json().then((data) => {
         if (response.status == 201) {
           console.log("Success")
@@ -43,8 +75,37 @@ const unendorsePin = (user: number, pid: number) =>{
           console.log("Internal Error")
         }
       })
+
     })
+
   }catch (e){
+    console.log("Error")
+    console.log(e)
+  }
+}
+
+const allValidates = (pid: string)=>{
+  try{
+    fetch(HOST + "/tasks/validates/" + pid, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "pid": pid,
+      },
+    }
+  ).then((response)=>{
+      response.json().then((data)=>{
+        if(response.status == 200){
+          console.log(data)
+          return data
+          console.log("Success")
+          //console.log(response)
+        }else{
+          console.log("Error")
+        }
+      })
+    })
+  } catch (e){
     console.log("Error")
     console.log(e)
   }
